fe434d79dd4ba589144d26fda3edd91a72ee2fe7
banjun <banjun@gmail.com>
2018-11-28 18:57:04 +0900

    Initial Commit

---
+++ Pencil2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Pencil2
+//
+//  Created by BAN Jun on 2018/11/28.
+//  Copyright © 2018 BAN Jun. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
